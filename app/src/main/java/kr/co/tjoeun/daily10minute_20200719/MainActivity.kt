7e10b172795199439a9cba061c5445276d785e30
조경진 <kj_cho@idealidea.co.kr>
2020-07-19 12:46:47 +0900

    로그인 실패시 토스트 띄우기

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
 import org.json.JSONObject
@@ -27,7 +28,25 @@ class MainActivity : BaseActivity() {
             ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 
+//                    code에 적힌 숫자가 몇인지 저장하자.
+                    val codeNum = json.getInt("code")
 
+//                    codeNum이 200일땐 로그인 성공
+//                    그 외의 모든 숫자는 로그인 실패.
+
+                    if (codeNum == 200) {
+//                        로그인 성공
+                    }
+                    else {
+//                        로그인 실패
+
+//                        서버 통신 중에 UI에 영향을 주려면 runOnUiThread 활용하자
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+
+
+                    }
 
                 }
 
