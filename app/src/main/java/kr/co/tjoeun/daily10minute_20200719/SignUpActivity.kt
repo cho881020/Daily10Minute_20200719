c5f5c789b435752d7c7ca9b81a4b3f1bc483974a
조경진 <kj_cho@idealidea.co.kr>
2020-07-19 15:12:56 +0900

    회원 가입 결과에 대한 처리

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/SignUpActivity.kt
@@ -65,6 +65,30 @@ class SignUpActivity : BaseActivity() {
             ServerUtil.putRequestSignUp(mContext, inputEmail, inputPw, inputNickName, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 
+//                    서버가 알려주는 코드값이 200이면 가입 성공처리.
+//                    아니라면 가입 실패 처리.
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        runOnUiThread {
+                            Toast.makeText(mContext, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+//                            로그인 화면으로 복귀
+                            finish()
+                        }
+                    }
+                    else {
+//                        가입 실패 상황. => 왜 실패했는지 토스트로 출력
+
+                        val message = json.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+
                 }
 
             })
