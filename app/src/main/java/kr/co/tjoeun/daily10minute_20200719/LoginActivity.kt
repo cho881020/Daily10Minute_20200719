38f86416b3f14fde1b0acdf3cdf6357a71a84c2c
조경진 <kj_cho@idealidea.co.kr>
2020-07-19 14:08:13 +0900

    액티비티 이름 변경 (LoingActivity)

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/LoginActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/LoginActivity.kt
@@ -1,17 +1,16 @@
 package kr.co.tjoeun.daily10minute_20200719
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
     }
