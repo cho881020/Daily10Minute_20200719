9cb932a9d0e274c83820ff9c933fe790a8a2b753
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 15:41:54 +0900

    참여중 사용자 파싱 + 목록에 반영 => 실 데이터 미반영

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.daily10minute_20200719
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_ongoing_users.*
+import kr.co.tjoeun.daily10minute_20200719.adapters.OngoingUserAdapter
 import kr.co.tjoeun.daily10minute_20200719.datas.Project
 import kr.co.tjoeun.daily10minute_20200719.datas.User
 import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
@@ -17,6 +18,8 @@ class ViewOngoingUsersActivity : BaseActivity() {
 
 //    사용자 정보를 저장할 목록
     val mOngoingUserList = ArrayList<User>()
+
+    lateinit var mOngoingUserAdapter : OngoingUserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +37,9 @@ class ViewOngoingUsersActivity : BaseActivity() {
         mProjectId = intent.getIntExtra("projectId", 0)
 
         getOngoingUsersFromServer()
+
+        mOngoingUserAdapter = OngoingUserAdapter(mContext, R.layout.ongoing_user_list_item, mOngoingUserList)
+        userListView.adapter = mOngoingUserAdapter
     }
 
 //    진행중인 사람 명단 + 상세정보 불러오기
@@ -50,12 +56,25 @@ class ViewOngoingUsersActivity : BaseActivity() {
 
 //                projectObj 내부에 ongoing_users 배열을 활용해서 사용자 명단을 목록에 저장
 
+                val ongoingUsers = projectObj.getJSONArray("ongoing_users")
+
+                for (i in 0 until ongoingUsers.length()) {
+                    val userObj = ongoingUsers.getJSONObject(i)
+
+                    val user = User.getUserFromJson(userObj)
+
+                    mOngoingUserList.add(user)
+                }
+
 
 //                프로젝트 정보 UI에 반영
 
                 runOnUiThread {
                     titleTxt.text = mProject.title
                     userCountTxt.text = "참여중 인원 : ${mProject.ongoingUserCount}명"
+
+//                    참여중인 사용자 명단도 반영
+                    mOngoingUserAdapter.notifyDataSetChanged()
                 }
 
             }
