967245ac8b0c528861d91767ad7ce731d170dd76
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 12:14:43 +0900

    실제 프로젝트 목록 데이터 반영

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ProjectAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ProjectAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10minute_20200719.R
 import kr.co.tjoeun.daily10minute_20200719.datas.Project
 
@@ -22,6 +25,17 @@ class ProjectAdapter(
         }
 
         val row = tempRow!!
+
+        val projectImg = row.findViewById<ImageView>(R.id.projectImg)
+        val projectTitleTxt = row.findViewById<TextView>(R.id.projectTitleTxt)
+        val projectDescriptionTxt = row.findViewById<TextView>(R.id.projectDescriptionTxt)
+
+        val data = mList[position]
+
+        projectTitleTxt.text = data.title
+        projectDescriptionTxt.text = data.description
+
+        Glide.with(mContext).load(data.imageUrl).into(projectImg)
 
         return row
     }
