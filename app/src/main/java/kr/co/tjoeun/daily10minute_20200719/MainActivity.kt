da72a94f8a58f0345c6f7616d0a95c15fc7fe007
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 11:13:42 +0900

    프로젝트 목록 서버에서 불러오기

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/MainActivity.kt
@@ -2,8 +2,13 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.daily10minute_20200719.datas.Project
+import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mProjectList = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,23 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        서버에서 받아오는 기능 실행
+        getProjectListFromServer()
+
+
+    }
+
+//    서버에서 프로젝트 목록이 어떤게 있는지 요청해서 받아주는 함수
+
+    fun getProjectListFromServer() {
+
+        ServerUtil.getRequestProjectList(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 
