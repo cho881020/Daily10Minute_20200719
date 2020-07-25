6ecbce0d812528af134a92dda9926c6414910ef6
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 12:42:43 +0900

    프로젝트 상세 정보 화면 초안 그림

---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectDetailActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minute_20200719
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProjectDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_project_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
