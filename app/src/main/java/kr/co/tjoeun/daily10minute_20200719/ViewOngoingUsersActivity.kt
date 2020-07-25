b3c78acde11392638011921ce8f7e361486ff827
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 14:47:58 +0900

    프로젝트 정보 파싱 => UI 반영  - 명단을 실제로 뿌려줘야함

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
@@ -2,12 +2,17 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_ongoing_users.*
+import kr.co.tjoeun.daily10minute_20200719.datas.Project
 import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
 import org.json.JSONObject
 
 class ViewOngoingUsersActivity : BaseActivity() {
 
     var mProjectId = 0
+
+//    서버에서 받아온 프로젝트 정보
+    lateinit var mProject : Project
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +38,18 @@ class ViewOngoingUsersActivity : BaseActivity() {
 
         ServerUtil.getRequestProjectDetailWithUsers(mContext, mProjectId, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val projectObj = data.getJSONObject("project")
+
+                mProject = Project.getProjectFromJson(projectObj)
+
+//                프로젝트 정보 UI에 반영
+
+                runOnUiThread {
+                    titleTxt.text = mProject.title
+                    userCountTxt.text = "참여중 인원 : ${mProject.ongoingUserCount}명"
+                }
 
             }
 
