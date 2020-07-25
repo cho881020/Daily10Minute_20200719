231a1e6f9a4f63c204526cac080194d1eab37ac3
조경진 <kj_cho@idealidea.co.kr>
2020-07-25 10:32:40 +0900

    프로젝트 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ProjectAdapter.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.daily10minute_20200719.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.daily10minute_20200719.R
+import kr.co.tjoeun.daily10minute_20200719.datas.Project
+
+class ProjectAdapter(
+    val mContext:Context,
+    resId:Int,
+    val mList:List<Project>) : ArrayAdapter<Project>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.project_list_view, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
