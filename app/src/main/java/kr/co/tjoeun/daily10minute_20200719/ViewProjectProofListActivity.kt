017b1e47710099f8371745f94f30a1d602a47b28
조경진 <kj_cho@idealidea.co.kr>
2020-07-26 12:51:19 +0900

    인증 목록 서버에 요청하기

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectProofListActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectProofListActivity.kt
@@ -1,17 +1,26 @@
 package kr.co.tjoeun.daily10minute_20200719
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_project_proof_list.*
+import kr.co.tjoeun.daily10minute_20200719.datas.Proof
+import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ViewProjectProofListActivity : BaseActivity() {
 
+//    몇번 프로젝트에 대한 인증 목록인지
+    var mProjectId = 0
+
+//    인증 게시글들이 담길 목록
+    val mProofList = ArrayList<Proof>()
+
 //    인증을 확인할 날짜를 저장해주는 변수
 //    proofDate => 기본값이 현재 시간으로 저장됨.
-    val proofDate = Calendar.getInstance()
+    val mProofDate = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,21 +38,21 @@ class ViewProjectProofListActivity : BaseActivity() {
             val datePickerDialog = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
 
 //                인증 날짜를 선택된 날짜로 변경
-                proofDate.set(Calendar.YEAR, year)
-                proofDate.set(Calendar.MONTH, month)
-                proofDate.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                mProofDate.set(Calendar.YEAR, year)
+                mProofDate.set(Calendar.MONTH, month)
+                mProofDate.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
 
 //                변경된 인증 날짜를 화면에 반영
                 val sdf = SimpleDateFormat("yyyy년 M월 d일")
-                proofDateTxt.text = sdf.format(proofDate.time)
+                proofDateTxt.text = sdf.format(mProofDate.time)
 
 //                변경된 날짜의 인증 내역 가져오기
                 getProofListFromServer()
 
 
 
-            }, proofDate.get(Calendar.YEAR), proofDate.get(Calendar.MONTH), proofDate.get(Calendar.DAY_OF_MONTH))
+            }, mProofDate.get(Calendar.YEAR), mProofDate.get(Calendar.MONTH), mProofDate.get(Calendar.DAY_OF_MONTH))
 
             datePickerDialog.show()
 
@@ -53,12 +62,16 @@ class ViewProjectProofListActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        이전 화면에서 넘겨준 프로젝트 id값을 멤버변수에 저장
+        mProjectId = intent.getIntExtra("projectId", 0)
+
+
 //        proofDate에 담긴 일시 (Calendar) 를 => 2020년 6월 9일 형태로 문구 (String) 출력.
 //        SimpleDateFormat의 기능중 format 사용
 
         val sdf = SimpleDateFormat("yyyy년 M월 d일")
 
-        val proofDateStr = sdf.format(proofDate.time)
+        val proofDateStr = sdf.format(mProofDate.time)
 
         proofDateTxt.text = proofDateStr
 
@@ -72,7 +85,16 @@ class ViewProjectProofListActivity : BaseActivity() {
 
     fun getProofListFromServer() {
 
+//        선택해둔 날짜를 => 2020-06-08 양식의 String으로 가공
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        val dateStr = sdf.format(mProofDate.time)
 
+        ServerUtil.getRequestProjectDetailWithProofList(mContext, mProjectId, dateStr, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 
