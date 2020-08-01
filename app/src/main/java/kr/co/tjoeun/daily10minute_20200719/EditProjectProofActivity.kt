5d5a0b0ec0c51e4b0aa1a0908101a721edc1a28d
조경진 <kj_cho@idealidea.co.kr>
2020-08-01 12:46:09 +0900

    프로젝트 참여 인증하는 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.daily10minute_20200719
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProjectProofActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_project_proof)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
