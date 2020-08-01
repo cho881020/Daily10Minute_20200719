13fa2806e9db9099f0c581fce1714655848973e5
조경진 <kj_cho@idealidea.co.kr>
2020-08-01 11:12:32 +0900

    인증 상세 화면 서버 데이터 표시

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProofDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProofDetailActivity.kt
@@ -1,11 +1,19 @@
 package kr.co.tjoeun.daily10minute_20200719
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_proof_detail.*
+import kr.co.tjoeun.daily10minute_20200719.datas.Proof
+import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
+import kr.co.tjoeun.daily10minute_20200719.utils.TimeUtil
+import org.json.JSONObject
 
 class ViewProofDetailActivity : BaseActivity() {
 
 //    목록에서 넘겨주는 인증글의 id값
     var mProofId = 0
+
+//    이 화면에서 표시해야할 데이터들이 담긴 인증 글
+    lateinit var mProof : Proof
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,25 @@ class ViewProofDetailActivity : BaseActivity() {
 
     fun getProofDataFromServer() {
 //        GET - /project_proof/id값   API 호출
+
+        ServerUtil.getRequestProofDetail(mContext, mProofId, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val proof = data.getJSONObject("project")
+
+                mProof = Proof.getProofFromJson(proof)
+
+                runOnUiThread {
+                    writerNickNameTxt.text = mProof.user.nickName
+                    createAtTxt.text = TimeUtil.getTimeAgoStringFromCalendar(mProof.proofTime)
+                    contentTxt.text = mProof.content
+                }
+
+            }
+
+        })
+
     }
 
 }
--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/utils/ServerUtil.kt
@@ -360,6 +360,68 @@ class ServerUtil {
 
         }
 
+
+//        인증 게시글의 상세정보를 불러오는 기능
+
+        fun getRequestProofDetail(context: Context, proofId: Int, handler: JsonResponseHandler?) {
+
+//            서버에 Request를 날려주는 변수
+            val client = OkHttpClient()
+
+//            GET / DELETE방식의 파라미터 첨부는 query에 담아야 함.
+//            주소에 붙여주는 방식 => 쉽게 가공하도록 도와주는 변수 생성.
+            val urlBuilder = "${BASE_URL}/project_proof/${proofId}".toHttpUrlOrNull()!!.newBuilder()
+//            urlBuilder.addEncodedQueryParameter("이름표", "전달값")
+
+
+//            주소 가공이 끝나면 최종 String으로 변환.
+//            어디로 갈지 (url) + 어떤 데이터를 가져갈지 (parameter) 첨부된 String
+            val urlString = urlBuilder.build().toString()
+
+
+//            실제 전송 정보를 담는 Request 생성
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getLoginUserToken(context))
+                .build()
+
+
+//            client변수를 이용해서 실제 호출 => 응답 처리
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                    서버 연결 자체에 실패한 경우 (단선, 와이파이 끊김, 서버 다운)
+//                    어떤 일이 생겨서 실패했는지 로그 출력
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    서버 연결 자체는 성공 => 원하는 결과를 얻었다는 보장은 없다.
+//                    응답 내용 (String)을 분석(JSONObject)해서 => 화면에 반영.
+
+//                    응답 내용을 String으로 저장
+                    val bodyString = response.body!!.string()
+
+//                    저장한 String을 가지고 => JSONObject로 재가공 (분석의 편의성)
+
+                    val json = JSONObject(bodyString)
+
+//                    가공된 JSON을 로그로 출력
+                    Log.d("서버 응답", json.toString())
+
+//                    JSON내용 분석은 => 화면에서 진행하게 넘겨주자.
+                    handler?.onResponse(json)
+
+                }
+
+            })
+
+
+
+        }
+
+
 //        프로젝트의 상세정보 + 참여중 인원 목록 API 호출 기능
 
         fun getRequestProjectDetailWithUsers(context: Context, projectId: Int, handler: JsonResponseHandler?) {
