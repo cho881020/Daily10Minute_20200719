cbceba0f1604de5650fc240480fdb1f702a2b77e
조경진 <kj_cho@idealidea.co.kr>
2020-08-01 13:46:56 +0900

    프로젝트 인증하기 화면 진입  - 필요 데이터들 전달 / 표시

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_project_proof.*
 
 class EditProjectProofActivity : BaseActivity() {
+
+//    어떤 프로젝트에 대해 인증할지 구별하는 id값
+    var mProjectId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,11 @@ class EditProjectProofActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProjectId = intent.getIntExtra("projectId", 0)
+
+//        프로젝트의 제목은 intent가 넘겨주는 내용을 그대로 표시
+        projectTitleTxt.text = intent.getStringExtra("projectTitle")
 
     }
 
--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewProjectDetailActivity.kt
@@ -30,6 +30,20 @@ class ViewProjectDetailActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        uploadProofBtn.setOnClickListener {
+
+//            인증 등록하기 화면에서도 어떤 프로젝트에 대한 인증인지 표시
+//            프로젝트의 id값 + 프로젝트의 제목을 둘다 전달해서 단순히 표시.
+
+            val myIntent = Intent(mContext, EditProjectProofActivity::class.java)
+
+            myIntent.putExtra("projectId", mProjectId)
+            myIntent.putExtra("projectTitle", mProject.title)
+
+            startActivity(myIntent)
+
+        }
+
 //        다른 사람들의 참여 인증 확인하는 버튼
         viewOtherProofBtn.setOnClickListener {
 
