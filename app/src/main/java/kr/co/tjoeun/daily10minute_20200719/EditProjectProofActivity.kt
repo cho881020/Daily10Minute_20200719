cf6f48afd024f8ad23e388ac78e776daacc3d596
조경진 <kj_cho@idealidea.co.kr>
2020-08-01 13:53:48 +0900

    인증글 등록시 길이 검사 / 최종 업로드 의사 확인

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/EditProjectProofActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.daily10minute_20200719
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_edit_project_proof.*
 
 class EditProjectProofActivity : BaseActivity() {
@@ -17,6 +20,32 @@ class EditProjectProofActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        postProofBtn.setOnClickListener {
+
+            val input = proofContentEdt.text.toString()
+
+            if (input.length < 5) {
+                Toast.makeText(mContext, "인증 내용은 5자 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            하루에 한번만 올릴 수 있으니 => 진짜 올릴건지 확인받고 올리자.
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("오늘의 인증 하기")
+            alert.setMessage("인증글은 하루에 한번만 올릴 수 있습니다. 정말 인증 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+
+//                의사가 확인 되었으니 => 서버에 게시글을 올리자.
+
+
+
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
 
     }
 
