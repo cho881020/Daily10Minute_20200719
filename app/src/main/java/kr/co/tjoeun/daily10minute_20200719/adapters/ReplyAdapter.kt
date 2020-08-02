3b15e341f588f15c42aeae161847685966d92495
조경진 <kj_cho@idealidea.co.kr>
2020-08-02 10:19:13 +0900

    내 좋아요 여부에 따른 버튼 모양/문구 변경

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ReplyAdapter.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.daily10minute_20200719.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
@@ -46,6 +47,28 @@ class ReplyAdapter(
         else {
             likeCountTxt.visibility = View.VISIBLE
             likeCountTxt.text = "좋아요 ${data.likeCount}개"
+        }
+
+//        내 좋아요 여부에 따른 처리
+//        myLike가 true : 빨간 테두리 + 빨간 글씨 (naverRed) + 좋아요 취소
+//        반대 : 회색 테두리 + 검정 글씨 + 좋아요
+
+        if (data.myLike) {
+//            테두리 처리 => xml로 그려둔 drawable 활용
+            likeBtn.setBackgroundResource(R.drawable.red_border_box)
+
+//            빨간 글씨 => res > colors.xml 에 있는 항목 (naverRed) 가져오기
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.naverRed))
+
+//            좋아요 취소로 문구 변경
+            likeBtn.text = "좋아요 취소"
+
+        }
+        else {
+            likeBtn.setBackgroundResource(R.drawable.gray_border_box)
+//            검정 글씨 => 기본색에서 BLACK 지원 (기본 색 사용법 예시)
+            likeBtn.setTextColor(Color.BLACK)
+            likeBtn.text = "좋아요"
         }
 
         likeBtn.setOnClickListener {
--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/datas/Reply.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/datas/Reply.kt
@@ -8,6 +8,7 @@ class Reply {
     var content = ""
     lateinit var writer : User
     var likeCount = 0
+    var myLike = false
 
     companion object {
 
@@ -17,6 +18,7 @@ class Reply {
             r.id = json.getInt("id")
             r.content = json.getString("content")
             r.likeCount = json.getInt("like_count")
+            r.myLike = json.getBoolean("my_like")
 
 //            user JSONObject 추출 => User 클래스로 변환 => r.writer에 대입
             r.writer = User.getUserFromJson(json.getJSONObject("user"))
